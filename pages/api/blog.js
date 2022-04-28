c4cfdc3e7cedc9d1f848a0035a1dc43742dfb453
unknown <kandelsanjay1111@gmail.com>
2022-04-28 17:42:19 +0545

    readfile and directory using fs

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 
 export default function handler(req, res) {
     fs.readFile(".././test/data/blog.json",(error,data)=>{
+        console.log(typeof(data))
         res.status(200).json(data)
     })
   }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Console } from 'console';
+import * as fs from 'fs';
 
 export default function handler(req, res) {
-  console.log(req);
-  res.status(200).json({ name: 'John Doe' })
+  let allblogs=[];
+
+  fs.readdir(".././test/data",(err,blog)=>{
+    // console.log(blog);
+    blog.forEach((item)=>{
+      // console.log(item);
+        fs.readFile(".././test/data/"+item,"utf-8",(error,data)=>{
+           allblogs.push(JSON.parse(data));
+      })
+    })
+    console.log(allblogs);
+    res.status(200).json({name:'test'})
+  })
+
 }
