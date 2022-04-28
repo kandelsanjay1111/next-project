50ea4747904cf351cf744f4606c0af10437cb58c
unknown <kandelsanjay1111@gmail.com>
2022-04-28 14:09:45 +0545

    fixed path problem in blog.js

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 
 export default function handler(req, res) {
-    fs.readFile(".././data/blog.json",(error,data)=>{
-        console.log(data);
+    fs.readFile(".././test/data/blog.json",(error,data)=>{
+        console.log(data)
     })
     res.status(200).json({ name: 'John Doe' })
   }
