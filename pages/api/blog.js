bfe6e85ea93f9995775ab713969d9a0110c991c5
unknown <kandelsanjay1111@gmail.com>
2022-04-28 15:29:01 +0545

    api created for blogs

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -2,7 +2,6 @@ import * as fs from 'fs';
 
 export default function handler(req, res) {
     fs.readFile(".././test/data/blog.json",(error,data)=>{
-        console.log(data)
+        res.status(200).json(data)
     })
-    res.status(200).json({ name: 'John Doe' })
   }
