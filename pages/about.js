2de6dd2f3890ed6de867cc3289180577254f5818
unknown <kandelsanjay1111@gmail.com>
2022-06-01 08:24:35 +0545

    client side data fetching with swr

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import useSwr from 'swr'
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link';
+
 
 export default function about() {
+  const { data, error } = useSwr('http://localhost:3000/api/blog', fetcher)
+  if(error) return <div>Error in fetching data</div>
+  if(!data) return <div>Loading...</div>
   return (
-    <div>About component</div>
+    <main className={styles.main}>
+        {
+            data.map((item,index)=>{
+                return <div className='blogItem' key={index}>
+                    <Link href="/blog/1"><h3 className={styles.blog_title}>{item.title}</h3></Link>
+                    <p>{item.content}</p></div>
+            })
+        }
+        
+        
+    </main>
   )
 }
