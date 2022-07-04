e7ce1f1d13b10c9c2551224d386f569d63221652
unknown <kandelsanjay1111@gmail.com>
2022-07-04 14:44:47 +0545

    added delete blog functionality

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,9 @@
 import React,{useEffect} from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
-import {useQuery} from 'react-query'
+import {useQuery} from 'react-query';
+import {db,app} from './firebase';
+import {doc,deleteDoc} from '@firebase/firestore';
 
 // export const getServerSideProps=async ()=>{
 // const res= await fetch('https://jsonplaceholder.typicode.com/posts/');
@@ -12,13 +14,14 @@ import {useQuery} from 'react-query'
 //     }
 // }
 // }  
+
 const fetchBlog=async()=>{
   const response=await fetch('http://localhost:3000/api/blog');
   return await response.json();
 };
 
 export default function blog() {
-  const {isLoading,data,isError,error}=useQuery('blogs',fetchBlog);
+  const {isLoading,data,isError,refetch}=useQuery('blogs',fetchBlog);
 
   if(isLoading){
     return <div>Loading....</div>
@@ -27,15 +30,28 @@ export default function blog() {
   if(isError){
     return <div>error in link...</div>
   }
+
+  const handleDelete=(e,id)=>{
+    // alert(id);
+    const collectionRef = doc(db, 'blog', id);
+    deleteDoc(collectionRef)
+            .then(() => {
+                refetch();
+            });
+
+    console.log(collectionRef);
+  };
   
   return (
     <>
     <main className={styles.main}>
+      <button>Add new</button>
         <div className={styles.blog_content}>
         {
             data.map((item,index)=>{
                 return <div className='blogItem' key={index}>
                     <Link href={`blog/${item.id}`}><h3 className={styles.blog_title}>{item.title}</h3></Link>
+                    <button onClick={()=>handleDelete(event,item.id)}>Delete</button>
                     <p>{item.content}</p></div>
             })
         }
